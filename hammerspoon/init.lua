cd1c5065637085f1be00232cc84aab0832448a80
Mike Chen <517926804@qq.com>
2022-05-09 14:49:16 +0800

    Format Code

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,18 +5,18 @@ require "hotkey"
 
 function reload(paths)
   local doReload = false
-    for _, file in pairs(paths) do
-        if file:sub(-4) == ".lua" then
-            print("A lua config file changed, reload")
-            doReload = true
-        end
+  for _, file in pairs(paths) do
+    if file:sub(-4) == ".lua" then
+      print("A lua config file changed, reload")
+      doReload = true
     end
-    if not doReload then
-        print("No lua file changed, skipping reload")
-        return
-    end
+  end
+  if not doReload then
+    print("No lua file changed, skipping reload")
+    return
+  end
 
-    hs.reload()
+  hs.reload()
 end
 
 local config_file_watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload)
