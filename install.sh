9ce100756db33e6bc0c80c91db0c7755a9429329
Mike Chen <517926804@qq.com>
2022-05-30 20:55:46 +0800

    Use your own fork's Doom-Emacs

--- install.sh
+++ install.sh
@@ -171,7 +171,7 @@ if [ ! -d "$EMACSD" ] || [ ! -d "$EMACSDOOMD" ] || [ ! -d "$DOOMD" ]; then
     ln -snf "$DOTFILES"/emacs-profiles.el "$HOME"/.emacs-profiles.el
 
     sync_git_repo github plexus/chemacs2 "$EMACSD"
-    sync_git_repo github hlissner/doom-emacs "$EMACSDOOMD" develop
+    sync_git_repo github cxb811201/doom-emacs "$EMACSDOOMD" tree-sitter
     sync_git_repo github cxb811201/.doom.d "$DOOMD"
     if cmd_exists "emacs"; then
         "$EMACSDOOMD"/bin/doom install --no-config --no-env
