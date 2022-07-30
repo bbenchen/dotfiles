e9d95d0f48ac4e9c19816d2b374a7f17a8aaeaf5
Mike Chen <517926804@qq.com>
2022-07-30 13:09:17 +0800

    Update nvim config

--- config/nvim/lua/my/plugins.lua
+++ config/nvim/lua/my/plugins.lua
@@ -81,7 +81,6 @@ return packer.startup(function(use)
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
   }
   use "JoosepAlviste/nvim-ts-context-commentstring"
 
