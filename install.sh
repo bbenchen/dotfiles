ccd5d4044d86ba164cb0889250ce8febaf30fb3b
Mike Chen <517926804@qq.com>
2023-08-22 15:58:15 +0800

    Update gnupg configuration

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ fi
 print_info "installing dotfiles..."
 # common
 cp -rf "$DOTFILES"/gnupg "$HOME"/.gnupg
+echo "$DOOMD"/bin/pinentry-emacs >> "$HOME"/.gnupg/gpg-agent.conf
 ln -snf "$DOTFILES"/authinfo.gpg "$HOME"/.authinfo.gpg
 ln -snf "$DOTFILES"/mbsyncrc "$HOME"/.mbsyncrc
 ln -snf "$DOTFILES"/msmtprc "$HOME"/.msmtprc
