a2a0b3cb97389ee82729269a8efdb8d7a2020e79
Ben Chen <517926804@qq.com>
2024-05-17 11:44:27 +0800

    Add ssh configuration

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ fi
 
 print_info "installing dotfiles..."
 # common
+cp -rf "$DOTFILES"/ssh "$HOME"/.ssh
 cp -rf "$DOTFILES"/gnupg "$HOME"/.gnupg
 # echo "$DOOMD"/bin/pinentry-emacs >> "$HOME"/.gnupg/gpg-agent.conf
 ln -snf "$DOTFILES"/authinfo.gpg "$HOME"/.authinfo.gpg
