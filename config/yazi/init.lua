57a304e363a00b85325d531eba8300616407a64f
Ben Chen <517926804@qq.com>
2024-08-06 09:38:51 +0800

    Compatible with the latest Yazi

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,36 +1,11 @@
-function Manager:render(area)
-    local chunks = self:layout(area)
-
-    local bar = function(c, x, y)
-        x, y = math.max(0, x), math.max(0, y)
-        return ui.Bar(ui.Rect { x = x, y = y, w = ya.clamp(0, area.w - x, 1), h = math.min(1, area.h) }, ui.Bar.TOP)
-            :symbol(c)
-    end
-
-    return ya.flat {
-        -- Borders
-        ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
-        ui.Bar(chunks[1], ui.Bar.RIGHT),
-        ui.Bar(chunks[3], ui.Bar.LEFT),
-
-        bar("┬", chunks[1].right - 1, chunks[1].y),
-        bar("┴", chunks[1].right - 1, chunks[1].bottom - 1),
-        bar("┬", chunks[2].right, chunks[2].y),
-        bar("┴", chunks[2].right, chunks[1].bottom - 1),
-
-        -- Parent
-        Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
-        -- Current
-        Current:render(chunks[2]:padding(ui.Padding.y(1))),
-        -- Preview
-        Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
-    }
-end
+require("full-border"):setup {
+    type = ui.Border.ROUNDED,
+}
 
 function Status:name()
-    local h = cx.active.current.hovered
+    local h = self._tab.current.hovered
     if not h then
-        return ui.Span("")
+        return ui.Line {}
     end
 
     local linked = ""
@@ -40,7 +15,7 @@ function Status:name()
     return ui.Span(" " .. h.name .. linked)
 end
 
-function Status:owner()
+Status:children_add(function()
     local h = cx.active.current.hovered
     if h == nil or ya.target_family() ~= "unix" then
         return ui.Line {}
@@ -52,16 +27,4 @@ function Status:owner()
         ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
         ui.Span(" "),
     }
-end
-
-function Status:render(area)
-    self.area = area
-
-    local left = ui.Line { self:mode(), self:size(), self:name() }
-    local right = ui.Line { self:owner(), self:permissions(), self:percentage(), self:position() }
-    return {
-        ui.Paragraph(area, { left }),
-        ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-        table.unpack(Progress:render(area, right:width())),
-    }
-end
+end, 500, Status.RIGHT)
