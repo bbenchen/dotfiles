b6659b289d4664b54086fbf3f6b588e86bcc6108
Ben Chen <517926804@qq.com>
2024-09-02 16:40:29 +0800

    Update proxy

--- utils.sh
+++ utils.sh
@@ -260,8 +260,8 @@ if [ "$(get_os)" = "macos" ]; then
 fi
 
 function open_proxy() {
-    export http_proxy="http://127.0.0.1:7890"
-    export https_proxy="http://127.0.0.1:7890"
+    export http_proxy="http://127.0.0.1:20122"
+    export https_proxy="http://127.0.0.1:20122"
     # export all_proxy="socks5://127.0.0.1:1089"
     export no_proxy="localhost,arch,127.0.0.1,10.0.0.*,172.16.*.*,192.168.*.*"
     echo "HTTP Proxy on"
