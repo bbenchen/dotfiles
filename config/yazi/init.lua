7d16ee8f06daa09080eab7393857466771c8d940
Ben Chen <517926804@qq.com>
2024-12-18 10:34:30 +0800

    Replace tab with a space

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -8,22 +8,22 @@ require("folder-rules"):setup()
 require("git"):setup()
 
 function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
-	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = os.date("%b %d  %Y", time)
-	end
+    local time = math.floor(self._file.cha.mtime or 0)
+    if time == 0 then
+        time = ""
+    elseif os.date("%Y", time) == os.date("%Y") then
+        time = os.date("%b %d %H:%M", time)
+    else
+        time = os.date("%b %d  %Y", time)
+    end
 
-	local size = self._file:size()
-	if size then
-		return string.format("%s %s", ya.readable_size(size), time)
-	else
-		local folder = cx.active:history(self._file.url)
-	    return string.format("%s %s", folder and tostring(#folder.files) or "-", time)
-	end
+    local size = self._file:size()
+    if size then
+        return string.format("%s %s", ya.readable_size(size), time)
+    else
+        local folder = cx.active:history(self._file.url)
+        return string.format("%s %s", folder and tostring(#folder.files) or "-", time)
+    end
 end
 
 function Status:name()
