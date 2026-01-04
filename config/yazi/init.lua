7309029fb6053e687dea24c21213837e88f7db6b
Ben Chen <517926804@qq.com>
2026-01-04 16:29:02 +0800

    Adapted for the latest version of Yazi

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -16,99 +16,6 @@ require("copy-file-contents"):setup({
     notification = true,
 })
 
-require("yatline"):setup({
-    --theme = my_theme,
-    section_separator = { open = "", close = "" },
-    part_separator = { open = "", close = "" },
-    inverse_separator = { open = "", close = "" },
-
-    style_a = {
-        fg = "black",
-        bg_mode = {
-            normal = "white",
-            select = "brightyellow",
-            un_set = "brightred"
-        }
-    },
-    style_b = { bg = "brightblack", fg = "brightwhite" },
-    style_c = { bg = "black", fg = "brightwhite" },
-
-    permissions_t_fg = "green",
-    permissions_r_fg = "yellow",
-    permissions_w_fg = "red",
-    permissions_x_fg = "cyan",
-    permissions_s_fg = "white",
-
-    tab_width = 20,
-    tab_use_inverse = false,
-
-    selected = { icon = "󰻭", fg = "yellow" },
-    copied = { icon = "", fg = "green" },
-    cut = { icon = "", fg = "red" },
-
-    total = { icon = "󰮍", fg = "yellow" },
-    succ = { icon = "", fg = "green" },
-    fail = { icon = "", fg = "red" },
-    found = { icon = "󰮕", fg = "blue" },
-    processed = { icon = "󰐍", fg = "green" },
-
-    show_background = true,
-
-    display_header_line = true,
-    display_status_line = true,
-
-    component_positions = { "header", "tab", "status" },
-
-    header_line = {
-        left = {
-            section_a = {
-                    {type = "line", custom = false, name = "tabs", params = {"left"}},
-            },
-            section_b = {
-            },
-            section_c = {
-            }
-        },
-        right = {
-            section_a = {
-                    {type = "string", custom = false, name = "date", params = {"%A, %d %B %Y"}},
-            },
-            section_b = {
-                    {type = "string", custom = false, name = "date", params = {"%X"}},
-            },
-            section_c = {
-            }
-        }
-    },
-
-    status_line = {
-        left = {
-            section_a = {
-                    {type = "string", custom = false, name = "tab_mode"},
-            },
-            section_b = {
-                    {type = "string", custom = false, name = "hovered_size"},
-            },
-            section_c = {
-                    {type = "string", custom = false, name = "hovered_path"},
-                    {type = "coloreds", custom = false, name = "count"},
-            }
-        },
-        right = {
-            section_a = {
-                    {type = "string", custom = false, name = "cursor_position"},
-            },
-            section_b = {
-                    {type = "string", custom = false, name = "cursor_percentage"},
-            },
-            section_c = {
-                    {type = "string", custom = false, name = "hovered_file_extension", params = {true}},
-                    {type = "coloreds", custom = false, name = "permissions"},
-            }
-        }
-    },
-})
-
 function Linemode:size_and_mtime()
     local time = math.floor(self._file.cha.mtime or 0)
     if time == 0 then
@@ -127,3 +34,17 @@ function Linemode:size_and_mtime()
         return string.format("%s %s", folder and tostring(#folder.files) or "-", time)
     end
 end
+
+Status:children_add(function()
+    local h = cx.active.current.hovered
+    if not h or ya.target_family() ~= "unix" then
+        return ""
+    end
+
+    return ui.Line {
+        ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+        ":",
+        ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+        " ",
+    }
+end, 500, Status.RIGHT)
